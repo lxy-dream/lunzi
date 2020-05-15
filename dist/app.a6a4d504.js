ed6a3e6d2e1f15ff3c469379e2c642a0177dea0f
li <1505899522@qq.com>
2020-05-15 23:41:03 +0800

    添加close动画

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -13277,8 +13277,17 @@ exports.default = void 0;
 //
 //
 //
+//
+//
+//
 var _default = {
-  name: "GuluSider"
+  name: "GuluSider",
+  data: function data() {
+    return {
+      visible: true
+    };
+  },
+  methods: {}
 };
 exports.default = _default;
         var $c8317b = exports.default || module.exports;
@@ -13293,7 +13302,30 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "sider" }, [_vm._t("default")], 2)
+  return _c("transition", { attrs: { name: "slide" } }, [
+    _vm.visible
+      ? _c(
+          "div",
+          { staticClass: "sider" },
+          [
+            _vm._t("default"),
+            _vm._v(" "),
+            _c(
+              "button",
+              {
+                on: {
+                  click: function($event) {
+                    _vm.visible = false
+                  }
+                }
+              },
+              [_vm._v("close")]
+            )
+          ],
+          2
+        )
+      : _vm._e()
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
