828eab6e155f004866c96f6d9f317978e6af8b9b
li <1505899522@qq.com>
2020-05-16 16:58:11 +0800

    更新package.json

---
+++ index.js
@@ -0,0 +1,45 @@
+import Button from "./button";
+import Icon from "./icon";
+import ButtonGroup from "./button-group";
+import Input from './input'
+import Row from './row'
+import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
+import Tabs from './tabs'
+import TabsHead from './tabs-head'
+import TabsBody from './tabs-body'
+import TabsItem from './tabs-item'
+import TabsPane from './tabs-pane'
+import Popover from './popover'
+import Collapse from './collapse'
+import CollapseItem from './collapse-item'
+
+export {
+    Button,
+    Icon,
+    ButtonGroup,
+    Input,
+    Row,
+    Col,
+    Layout,
+    Header,
+    Sider,
+    Content,
+    Footer,
+    Toast,
+    plugin,
+    Tabs,
+    TabsHead,
+    TabsBody,
+    TabsItem,
+    TabsPane,
+    Popover,
+    Collapse,
+    CollapseItem
+}
